72d4bbfb99f641ba5193cd5257adf2f52aea79cb
AbhilashGaurav <84313712+AbhilashGaurav@users.noreply.github.com>
2023-01-21 20:00:03 +0530

    login and logout credential page

--- my_proj/base/views.py
+++ my_proj/base/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages 
 from django.http import HttpResponse
 from django.db.models import Q
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 
 from .forms import RoomForm
 # Create your views here.
 from .models import Room, Topic
-rooms = [
-    {'id': 1, 'name': '1 oop data'},
-    {'id': 2, 'name': '2 data'},
-    {'id': 3, 'name': '3 data'},
-    {'id': 4, 'name': '4 data'}
-]
+# rooms = [
+#     {'id': 1, 'name': '1 oop data'},
+#     {'id': 2, 'name': '2 data'},
+#     {'id': 3, 'name': '3 data'},
+#     {'id': 4, 'name': '4 data'}
+# ]
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Invalid username or password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    context = {}
+    return render(request,"base/login_page.html",context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') else ''
     rooms = Room.objects.filter(
