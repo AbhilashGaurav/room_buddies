c07f0e6bb45715198f8c9d8af4825a9813bb4e53
AbhilashGaurav <84313712+AbhilashGaurav@users.noreply.github.com>
2023-02-22 11:49:24 +0530

    conflict reslove

--- my_proj/base/admin.py
+++ my_proj/base/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
@@ -7,14 +6,4 @@ from .models import Room, Topic, Message
 admin.site.register(Room)
 
 admin.site.register(Topic)
-=======
-from django.contrib import admin
-
-# Register your models here.
-from .models import Room, Topic, Message
-
-admin.site.register(Room)
-
-admin.site.register(Topic)
->>>>>>> 7473195b0f73cdab975da524bc95679a4926a05b
 admin.site.register(Message)
